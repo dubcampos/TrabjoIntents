4158980d43a3dddefe7118f1669ca17d37bbda2b
dubcampos <duber_cch@hotmail.com>
2019-08-25 17:53:28 -0500

    Ejercicio 1 terminado
    Ejercicio 1 terminado

---
+++ app/src/main/java/com/example/trabjointents/Ejercicio1_1.java
@@ -0,0 +1,38 @@
+package com.example.trabjointents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class Ejercicio1_1 extends AppCompatActivity {
+
+    private TextView tv_usuario;
+    private Button btn_atras;
+    private Button btn_returnhome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio1_1);
+
+        tv_usuario = findViewById(R.id.tv_usuario);
+        btn_returnhome = findViewById(R.id.btn_returnhome);
+        btn_atras = findViewById(R.id.btn_atras);
+
+
+        String temporal = getIntent().getStringExtra("dato_nombre");
+        tv_usuario.setText("Bienvenido "+ temporal);
+
+    }
+
+    public void Atras(View view){
+        Intent atras = new Intent(this,MainActivity.class);
+        startActivity(atras);
+    }
+}
--- app/src/main/java/com/example/trabjointents/MainActivity.java
+++ app/src/main/java/com/example/trabjointents/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.trabjointents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_login;
+    private EditText et_usuario;
+    private Button btn_returnhome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_login = findViewById(R.id.btn_login);
+        et_usuario = findViewById(R.id.et_usuario);
+        btn_returnhome = findViewById(R.id.btn_returnhome);
     }
+
+    public void Login(View view){
+
+        Intent login = new Intent(this,Ejercicio1_1.class);
+        login.putExtra("dato_nombre", et_usuario.getText().toString());
+        startActivity(login);
+
+    }
+
 }
